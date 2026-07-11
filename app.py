f3126aed15d202143a7b94002b37b33eeaf63a74
Divyaprabha1424 <divyaprabha300@gmail.com>
2026-07-11 12:21:05 +0530

    Debug environment variables

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 # ==========================
 # MySQL Connection
 # ==========================
-
+print("DB_HOST =", os.getenv("DB_HOST"))
+print("DB_PORT =", os.getenv("DB_PORT"))
+print("DB_USER =", os.getenv("DB_USER"))
+print("DB_NAME =", os.getenv("DB_NAME"))
 
 db = pymysql.connect(
     host=os.getenv("DB_HOST"),
